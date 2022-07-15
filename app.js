77b85619e6b6841febbc3d27ecd31b79b11a838b
EdilsonRogerio <eddyrogerioyuran@gmail.com>
2022-07-15 17:36:34 -0300

    atualizando app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,6 +12,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+mongoose.connect("mongodb://localhost:27017/usarioDB");
+
+const esquemaUsuario = {
+    email: String,
+    password: String
+}
+
+const Usuario = new mongoose.model("Usuario", esquemaUsuario);
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -22,6 +32,21 @@ app.get("/login", function (req, res) {
 app.get("/registro", function (req, res) {
     res.render("registro");
 });
+
+app.post("/registro", function (req, res) {
+    const novoUsuario = new Usuario({
+        email: req.body.nomeusuario,
+        password: req.body.password
+    });
+
+    novoUsuario.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("segredo");
+        }
+    });
+})
 
 app.listen(3000, function (req, res) {
     console.log("Servidor inicou na porta 3000!");
