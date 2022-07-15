08ffffadcf55559347643d441030e2c87ef62a1f
EdilsonRogerio <eddyrogerioyuran@gmail.com>
2022-07-15 02:23:37 -0300

    atualizando arquivos

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.get("/", function (req, res) {
+    res.render("home");
+});
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/registro", function (req, res) {
+    res.render("registro");
+});
+
 app.listen(3000, function (req, res) {
     console.log("Servidor inicou na porta 3000!");
-})
+});
